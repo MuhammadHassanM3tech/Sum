521e4e800326ce930e9e21ec63e197675db87ff0
MuhammadHassanM3tech <Muhammad.Hassan@m3tech.com.pk>
2024-05-31 16:58:16 +0500

    Add project files.

---
+++ ConsoleApp1/Arithematic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Arithematic
+    {
+        
+        public int sum(int a , int b)
+        {
+            int sum1 = a + b;
+            return sum1;
+        }
+
+        public int sub(int a, int b)
+        {
+            int sub1 = a - b;
+            return sub1;
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            int y = 5;
+            Arithematic a = new Arithematic();
+            Console.WriteLine(a.sum(x,y));
+            Console.WriteLine(a.sub(x, y));
+
+        }
+    }
+}
